3b1f76b9e5be9e037292ee196bf6f338b6d1aef5
Vinit Kumar <44137151+vinitkr567@users.noreply.github.com>
2022-04-16 20:36:43 +0530

    main page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,9 +1,126 @@
-import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-const home = () => {
+export const Home = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    axios.get("http://localhost:8080/cities").then((res) => {
+      setData(res.data);
+    });
+  };
+
+  //delete
+  function handleDelete(e) {
+    console.log(e);
+    axios.delete(`http://localhost:8080/cities/${e}`);
+    getData();
+  }
+  //sort country
+
+  function handleCountry() {
+    data.sort(function (a, b) {
+      return a.country.localeCompare(b.country);
+    });
+    console.log(data);
+    setData([...data]);
+  }
+
+  //sort by population
+  function handlePopulation(value) {
+    if (value == 1) {
+      data.sort(function (a, b) {
+        return a.population - b.population;
+      });
+    } else {
+      data.sort(function (a, b) {
+        return b.population - a.population;
+      });
+    }
+    setData([...data]);
+  }
+
   return (
-    <div>home</div>
-  )
-}
+    <>
+      <div>
+        <h3>Filter By:</h3>
+        <button variant="text" onClick={handleCountry}>
+          Country
+        </button>
+        <button
+          variant="text"
+          onClick={() => {
+            handlePopulation(1);
+          }}
+        >
+          Population asc
+        </button>
+        <button
+          variant="text"
+          onClick={() => {
+            handlePopulation(-1);
+          }}
+        >
+          Population desc
+        </button>
+        <hr />
+      </div>
+      <div id="links">
+        <Link to={"/"} style={{ textDecoration: "none" }}>
+          Home
+        </Link>{" "}
+        <Link to={"/addcountry"} style={{ textDecoration: "none" }}>
+          Add Country
+        </Link>{" "}
+        <Link to={"/addcity"} style={{ textDecoration: "none" }}>
+          Add City
+        </Link>
+      </div>
+      <hr />
+      <div id="table">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Country</th>
+              <th>City</th>
+              <th>Population</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((e) => {
+              return (
+                <>
+                  <tr>
+                    <td>{e.id}</td>
+                    <td>{e.country}</td>
+                    <td>{e.cityname}</td>
+                    <td>{e.population}</td>
+                    <td>
+                      <button>Edit</button>
+                    </td>
 
-export default home
+                    <button
+                      onClick={() => {
+                        handleDelete(e.id);
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </tr>
+                </>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
