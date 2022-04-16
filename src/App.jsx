07d4dd18ad2187b5ff04a3422b3a12c45bf833ac
Vinit Kumar <44137151+vinitkr567@users.noreply.github.com>
2022-04-16 20:37:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
-import {AddCountry} from "./components/addcountry";
-import {AddCity} from "./components/addcity";
-import {home} from "./components/home";
-import {Routes, Route} from "react-router-dom";
+import { Addcity } from "./Components/addCity";
+import { Addcountry } from "./Components/addCountry";
+import { Home } from "./Components/home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Masai City</h1>
+      <Home />
       <Routes>
-        <Route path="/" element={<home />} />
-        <Route path="/add-country" element={<AddCountry />} />
-        <Route path="/add-city" element={<AddCity />} />
+        <Route path="/addcity" element={<Addcity />} />
+        <Route path="/addcountry" element={<Addcountry />} />
       </Routes>
     </div>
   );
